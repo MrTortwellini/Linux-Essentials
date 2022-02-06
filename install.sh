c3a036435b68c867ea7622242c01a580eb96a934
Dylan Allpress <45021029+MrTortwellini@users.noreply.github.com>
2022-02-06 14:41:29 +1300

    sorted from essentials vs non-essential

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@ inst="${key} install"
 rem="${key} remove"
 sn="sudo snap install"
 
-#autoremove removes package dependencies
-$key autoremove
-#parole is inferior to vlc
-$rem --purge parole
-#gnu compiler collection
-$inst gcc
-$inst vlc
+
+#ESSENTIALS
+
 #allow make commands
 $inst make
 #install tree obviously
 $inst tree
+#autoremove removes package dependencies
+$key autoremove
+#parole is inferior to vlc
+$rem --purge parole
 #remove any netcat files
 $rem --purge netcat-openBSD 
 #install netcat
@@ -28,9 +28,20 @@ $inst curl
 $inst openvpn
 #install git
 $inst git
+#gnu compiler collection
+$inst gcc
+#smbclient
+$inst smbclient
+#terminator
+$inst terminator
+
+#APPLICATIONS
+
+#vlc media player
+$inst vlc
 #simplenote
 wget https://github.com/Automattic/simplenote-electron/releases/download/v2.21.0/Simplenote-linux-2.21.0-amd64.deb
 #discord
 $sn discord
-#smbclient
-$inst smbclient
+
+
